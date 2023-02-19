a4ce2acf2cb07a9059bba4a903979069c3b0294b
Victor Gila Barreto <gilabarreto@gmail.com>
2023-02-19 15:05:29 -0700

    Clean, dry and comment App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 import ArtistPage from "./components/ArtistPage";
 import SearchPage from "./components/SearchPage";
@@ -17,72 +13,68 @@ import { faHeart, faMusic, faTrash } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 function App() {
-  const [setlist, setSetlist] = useState([]);
-  const [ticketmaster, setTicketmaster] = useState([]);
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [token, setToken] = useState("");
-  const [value, setValue] = useState("");
-  const [favourites, setFavourites] = useState([]);
-  const [loadingfavourites, setLoadingfavourites] = useState(false);
-  const [favouritesConcerts, setFavouritesConcerts] = useState([]);
-  const [favouritesTickets, setFavouritesTickets] = useState([]);
+  // Declare state variables and their initial values using the useState hook
+  const [setlist, setSetlist] = useState([]); // Array of setlists
+  const [ticketmaster, setTicketmaster] = useState([]); // Array of Ticketmaster data
+  const [lat, setLat] = useState([]); // Latitude
+  const [long, setLong] = useState([]); // Longitude
+  const [token, setToken] = useState(""); // Token for authentication
+  const [value, setValue] = useState(""); // Value of the search input
+  const [favourites, setFavourites] = useState([]); // Array of favourite artists
+  const [loadingfavourites, setLoadingfavourites] = useState(false); // Flag to indicate whether favourites are loading
+  const [favouritesConcerts, setFavouritesConcerts] = useState([]); // Array of concerts for each favourite artist
+  const [favouritesTickets, setFavouritesTickets] = useState([]); // Array of ticket information for each favourite artist
 
+  // Function to fetch data for a favourite artist
   const fetchDataByFavourite = (favourite) => {
-    const setlistPromise = axios
-      //GET Request Setlist
-      .get("/rest/1.0/search/setlists", {
-        params: {
-          artistName: `"${favourite.artistname}"`,
-          p: "1",
-        },
-        headers: {
-          Accept: "application/json",
-          "x-api-key": process.env.REACT_APP_SETLIST_KEY,
-        },
-      });
+    // Make a GET request to Setlist.fm API to get setlists for the artist
+    const setlistPromise = axios.get("/rest/1.0/search/setlists", {
+      params: {
+        artistName: `"${favourite.artistname}"`, // Use the artist name to search for setlists
+        p: "1", // Only get the first page of results
+      },
+      headers: {
+        Accept: "application/json",
+        "x-api-key": process.env.REACT_APP_SETLIST_KEY, // Use the API key for Setlist.fm
+      },
+    });
 
+    // Make a GET request to Ticketmaster API to get data for the artist
     const ticketmasterPromise = axios.get(
       "https://app.ticketmaster.com/discovery/v2/suggest",
       {
         params: {
-          keyword: `"${favourite.artistname}"`,
-          segmentId: "KZFzniwnSyZfZ7v7nJ",
-          sort: "name,asc",
-          apikey: process.env.REACT_APP_TICKETMASTER_KEY,
+          keyword: `"${favourite.artistname}"`, // Use the artist name to search for events
+          segmentId: "KZFzniwnSyZfZ7v7nJ", // Use the music segment ID
+          sort: "name,asc", // Sort the results by name in ascending order
+          apikey: process.env.REACT_APP_TICKETMASTER_KEY, // Use the API key for Ticketmaster
         },
       }
     );
 
     Promise.all([setlistPromise, ticketmasterPromise])
       .then(([setlistResponse, ticketmasterResponse]) => {
-        //Filters setlist data by concert date, only show concerts that already happened
-        const noUpcomingConcert = setlistResponse.data.setlist.filter(
-          (item) => {
-            const [day, month, year] = item.eventDate.split("-");
-            const date = new Date(year, month - 1, day);
-
-            if (Number(date) > Number(new Date())) {
-              return false;
-            }
-            return true;
-          }
-        );
+        // Filters setlist data by concert date, only show concerts that already happened
+        const noUpcomingConcert = setlistResponse.data.setlist.filter(item => {
+          const [year, month, day] = item.eventDate.split("-");
+          return Number(new Date(year, month - 1, day)) < Date.now();
+        });
 
         const uniqueIds = [];
 
-        //Filters the data for unique artists
+        // Filters the data for unique artists
         const uniqueSetlist = noUpcomingConcert.filter((item) => {
           const isDuplicate = uniqueIds.includes(item.artist.mbid);
 
           if (!isDuplicate) {
             uniqueIds.push(item.artist.mbid);
-
             return true;
           }
+
           return false;
         });
 
+        // Updates state with the favourite artist's name and last concert date
         setFavouritesConcerts((prev) => [
           ...prev,
           {
@@ -91,7 +83,7 @@ function App() {
           },
         ]);
 
-        //Find spotify link and image for specific artist
+        // Finds the Spotify link and image for the favourite artist
         const ticketmasterMap =
           ticketmasterResponse.data._embedded.attractions.find(
             (item) => item.name === favourite.artistname
@@ -106,24 +98,18 @@ function App() {
           spotify = ticketmasterMap.externalLinks.spotify[0].url;
         }
 
-        // Filter for only attractions from events
+        // Filters for only attractions from events and sorts by concert date
         const ticketmasterEvents = ticketmasterResponse.data._embedded.events
-          .filter((item) => {
-            if (item._embedded.attractions !== undefined) {
-              for (const attraction of item._embedded.attractions) {
-                if (attraction.name === favourite.artistname) {
-                  return item;
-                }
-              }
-            }
-          })
-          .sort((a, b) => a.dates.start.localDate - b.dates.start.localDate);
+          .flatMap(event => event._embedded.attractions || [])
+          .filter(attraction => attraction.name === favourite.artistname)
+          .sort((a, b) => a.dates.start.localDate.localeCompare(b.dates.start.localDate));
 
         let upcomingConcert = null;
-
         if (ticketmasterEvents[0] && ticketmasterEvents[0].dates) {
           upcomingConcert = ticketmasterEvents[0].dates.start.localDate;
         }
+
+        // Updates state with the favourite artist's name and upcoming concert date
         setFavouritesTickets((prev) => [
           ...prev,
           {
@@ -149,7 +135,6 @@ function App() {
       .then((res) => {
         setLoadingfavourites(false);
         setFavourites(res.data);
-        // res.data.map((item) => fetchDataByFavourite(item));
       })
       .catch(() => {
         setLoadingfavourites(false);
