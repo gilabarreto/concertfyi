ca06e655855b3251b4478bfbfaccc8300d60ad30
Victor Gila Barreto <gilabarreto@gmail.com>
2023-02-20 12:00:00 -0700

    Clean, dry and comment SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,43 +1,45 @@
 import React, { useEffect } from "react";
-
 import useDebounce from "../hooks/useDebounce";
-
 import { useNavigate, useParams } from "react-router-dom";
-
 import axios from "axios";
-
 import "./SearchBar.css";
 
 export default function SearchBar(props) {
+  // Get the artist ID from the URL using useParams hook
   let artistId = useParams();
 
   const { value, setValue } = props;
-
   const navigate = useNavigate();
 
+  // Function to handle changes to the search input
   const handleChange = (event) => {
+    // If there is an artist ID in the URL, navigate to the search page
     if (artistId) {
       navigate("/search");
     }
+    // Update the value of the search input
     setValue(event.target.value);
   };
 
+  // Debounce the value of the search input
   const term = useDebounce(value, 700);
 
+  // Function to fetch data from the Setlist API and the Ticketmaster API
   const fetchData = () => {
-    const setlistPromise = axios
-      //GET Request Setlist
-      .get("/rest/1.0/search/setlists", {
-        params: {
-          artistName: `"${value}"`,
-          p: "1",
-        },
-        headers: {
-          Accept: "application/json",
-          "x-api-key": process.env.REACT_APP_SETLIST_KEY,
-        },
-      });
+    // Send a GET request to the Setlist API with the artist name and page number as parameters
+    const setlistPromise = axios.get("/rest/1.0/search/setlists", {
+      params: {
+        artistName: `"${value}"`,
+        p: "1",
+      },
+      // Set the headers to include the API key
+      headers: {
+        Accept: "application/json",
+        "x-api-key": process.env.REACT_APP_SETLIST_KEY,
+      },
+    });
 
+    // Send a GET request to the Ticketmaster API with the search keyword, segment ID, sort order, and API key as parameters
     const ticketmasterPromise = axios.get(
       "https://app.ticketmaster.com/discovery/v2/suggest",
       {
@@ -52,6 +54,7 @@ export default function SearchBar(props) {
 
     Promise.all([setlistPromise, ticketmasterPromise])
       .then(([setlistResponse, ticketmasterResponse]) => {
+        // Update the setlist and ticketmaster state with the data returned from the API requests
         props.setSetlist(setlistResponse.data.setlist);
         props.setTicketmaster(ticketmasterResponse.data._embedded);
       })
@@ -60,10 +63,13 @@ export default function SearchBar(props) {
       });
   };
 
+  // useEffect hook to fetch data from the APIs whenever the term changes
   useEffect(() => {
+    // If the search input is empty, don't fetch data
     if (term.length === 0) {
       return;
     }
+    // Fetch data from the APIs
     fetchData();
   }, [term]);
 
