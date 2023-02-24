f2a47f16f6ef065ad56899a294eef5c3b0f6958d
Victor Gila Barreto <gilabarreto@gmail.com>
2023-02-24 12:00:00 -0700

    Clean, dry and add comment to Player.js

--- src/components/ArtistPage/Player.js
+++ src/components/ArtistPage/Player.js
@@ -1,13 +1,13 @@
-import axios from "axios";
 import SpotifyPlayer from "react-spotify-player";
 
+// Player component that takes in a ticketmaster object as props and displays a Spotify player
 export default function Player(props) {
-  const artist = props.concert.artist.name;
-
+  // Get the Spotify URL from the ticketmaster object, or set it to null if not available
   const spotify = props.ticketmaster.attractions
     ? props.ticketmaster.attractions[0].externalLinks.spotify[0].url
     : null;
 
+  // Set the size, view, and theme of the Spotify player
   const size = {
     width: "100%",
     height: 500,
@@ -15,6 +15,7 @@ export default function Player(props) {
   const view = "list"; // or 'coverart'
   const theme = "black"; // or 'white'
 
+  // Render the SpotifyPlayer component with the Spotify URI and settings as props
   return (
     <>
       <SpotifyPlayer uri={spotify} size={size} view={view} theme={theme} />
