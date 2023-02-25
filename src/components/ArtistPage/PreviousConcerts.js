74eefbfaeff4b866762c5b889ec31afaf9e94414
Victor Gila Barreto <gilabarreto@gmail.com>
2023-02-25 12:00:00 -0700

    Clean, dry and add comments to PreviousConcerts.js

--- src/components/ArtistPage/PreviousConcerts.js
+++ src/components/ArtistPage/PreviousConcerts.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 
+// PreviousConcerts component that takes in a setlist and artistId as props and displays a list of previous concerts
 export default function PreviousConcerts(props) {
-
-  const navigate = useNavigate()
+  // Use the useNavigate hook from the react-router-dom library to navigate to different pages
+  const navigate = useNavigate();
   
+  // Render a list of up to 10 previous concerts for the artist, filtered by artistId
   return (
     <>
       {
@@ -11,22 +13,31 @@ export default function PreviousConcerts(props) {
         props.setlist
           .filter((item) => item.artist.mbid === props.artistId)
           .map((concert) => {
+            // Parse the event date string into a date object
             const str = concert.eventDate;
             const [day, month, year] = str.split("-");
             const date = new Date(year, month - 1, day);
+            
+            // Define the options for the date format
             const options = {
               year: "numeric",
               month: "long",
               day: "numeric",
             };
+            
+            // Get the city, state, and country code for the venue
             const city = concert.venue.city?.name;
             const state = concert.venue.city?.state;
             const country = concert.venue.city?.country.code;
+            
+            // Only display concerts that have already happened
             if (new Date(date) < new Date()) {
               const concertLabel = `${date.toLocaleDateString(
                 "en-US",
                 options
               )} (${city}, ${state}, ${country})`;
+              
+              // Render a clickable span that navigates to the concert details page when clicked
               return (
                 <span className="prevConc-list" key={concertLabel}>
                   <span
@@ -41,6 +52,7 @@ export default function PreviousConcerts(props) {
               );
             }
           })
+          // Limit the list to the 10 most recent concerts
           .slice(0, 10)
       }
     </>
